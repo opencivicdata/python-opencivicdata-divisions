db601500b3cc80f3a9c1f4cf64b76705a82d3ced
James Turk <james.p.turk@gmail.com>
2017-05-24 20:56:51 -0700

    make package a redirect to python-opencivicdata

--- run-tests.sh
+++
@@ -1,1 +0,0 @@
-export PYTHONPATH=.; py.test --cov opencivicdata --cov-report html --cov-config=.coveragerc
--- setup.py
+++ setup.py
@@ -1,42 +1,14 @@
-#!/usr/bin/env python
-from setuptools import setup, __version__
-import sys
-
-# backports.csv required for Python 2.7
-INSTALL_REQUIRES = []
-EXTRAS_REQUIRE = {}
-
-# conditionally pass install_requires arg if setuptools older than v18
-if int(__version__.split(".", 1)[0]) < 18:
-    if sys.version_info[0:2] == (2, 7):
-        INSTALL_REQUIRES.append("backports.csv")
-# otherwise pass extra_requires arg
-else:
-    EXTRAS_REQUIRE[":python_version=='2.7'"] = ["backports.csv"]
+from setuptools import setup
 
 setup(name="opencivicdata-divisions",
-      version='2017.5.18',
-      py_modules=['opencivicdata.divisions'],
+      version='3000',       # pin to <3000 if you *need* a real version
       author="James Turk",
       author_email='james@openstates.org',
       license="BSD",
-      description="python opencivicdata library",
-      long_description="",
+      description="OBSOLETE - see opencivicdata package",
+      long_description="OBSOLETE - see opencivicdata package",
       url="",
-      packages=['opencivicdata.divisions'],
-      #namespace_packages=['opencivicdata'],
-      include_package_data=True,
-      install_requires=INSTALL_REQUIRES,
-      extras_require=EXTRAS_REQUIRE,
       platforms=["any"],
-      classifiers=["Development Status :: 4 - Beta",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Natural Language :: English",
-                   "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5",
-                   "Programming Language :: Python :: 3.6",
-                   ],
+      classifiers=["Development Status :: 7 - Inactive"],
+      install_requires=['opencivicdata'],   # redirect to real package
       )
